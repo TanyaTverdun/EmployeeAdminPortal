5f79ece474bd2b8879478a2df5a7f770fb2074dd
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2025-09-13 16:50:07 +0300

    Added structured models for AddEmployee (Dto, Request, Response, Mapper)

---
+++ EmployeeAdminPortal/Employees/AddEmployee/AddEmployeeMapper.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeAdminPortal.Employees.AddEmployee.Dtos;
+using EmployeeAdminPortal.Models.Inputs;
+using EmployeeAdminPortal.Models.Outputs;
+
+namespace EmployeeAdminPortal.Employees.AddEmployee
+{
+    public static class AddEmployeeMapper
+    {
+        public static AddEmployeeInput MapToInput(AddEmployeeRequest request)
+        {
+            return new AddEmployeeInput
+            {
+                Name = request.Employee.Name,
+                Email = request.Employee.Email,
+                Phone = request.Employee.Phone,
+                Salary = request.Employee.Salary,
+            };
+        }
+
+        public static AddEmployeeResponse MapToResponse(AddEmployeeOutput output)
+        {
+            return new AddEmployeeResponse
+            {
+                Employee = new EmployeeDto
+                {
+                    Name = output.Employee.Name,
+                    Email = output.Employee.Email,
+                    Phone = output.Employee.Phone,
+                    Salary = output.Employee.Salary
+                }
+            };
+        }
+    }
+}
